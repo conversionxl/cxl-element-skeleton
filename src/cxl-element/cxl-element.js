6a3cd7a3f602b277a8548a0dba61cb1025a0b220
pawel <pawel@conversionready.com>
2019-03-20 16:20:10 +0200

    CXLElement skeleton based on LitElement

---
+++ src/cxl-element/cxl-element.js
@@ -0,0 +1,34 @@
+// Import the LitElement base class and html helper function.
+import { LitElement, html } from 'lit-element';
+
+// Import styles.
+import styles from './cxl-element.scss';
+
+class CXLElement extends LitElement {
+  static get properties() {
+    return {};
+  }
+
+  /**
+   * Implement `render` to define a template for your element.
+   *
+   * You must provide an implementation of `render` for any element
+   * that uses LitElement as a base class.
+   */
+  render() {
+    /**
+     * `render` must return a lit-html `TemplateResult`.
+     *
+     * To create a `TemplateResult`, tag a JavaScript template literal
+     * with the `html` helper function:
+     */
+    return html`
+      <style>
+        ${styles}
+      </style>
+    `;
+  }
+}
+
+// Register the new element with the browser.
+customElements.define('cxl-element', CXLElement);
